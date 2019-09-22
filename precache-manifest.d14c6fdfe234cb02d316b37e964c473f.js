7b3a74b9c146bf206af4b4529504d188a15243c3
bliid <gh.bliid@gmail.com>
2019-09-22 19:38:31 +0100

    Updates

---
+++ precache-manifest.d14c6fdfe234cb02d316b37e964c473f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "598e0bec2afd279b7aaba69e6fba08dd",
+    "url": "/redux_movie/index.html"
+  },
+  {
+    "revision": "99b9baf49b7674ec8a6f",
+    "url": "/redux_movie/static/css/main.50b6168b.chunk.css"
+  },
+  {
+    "revision": "385c329e69077da113b7",
+    "url": "/redux_movie/static/js/2.60e98782.chunk.js"
+  },
+  {
+    "revision": "99b9baf49b7674ec8a6f",
+    "url": "/redux_movie/static/js/main.04c5c680.chunk.js"
+  },
+  {
+    "revision": "ddc20d77605809e1b735",
+    "url": "/redux_movie/static/js/runtime~main.96f0476c.js"
+  }
+]);
